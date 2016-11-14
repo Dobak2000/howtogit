07b4b336268a9d1352a27cd2f2e933071361b702
Dobak2000 <ragogumi2000@gmail.com>
2016-11-14 14:01:59 +0100

    Buzz remove

--- Fizz.py
+++ Fizz.py
@@ -1,5 +1,10 @@
 for i in range(100):
-    if (i + 1) % 3 == 0:
-   		print("Fizz")
+    if (i+1) % 3 == 0 and (i+1) % 4 == 0:
+        print("FIZZ BUZZ")
+    
+    elif (i+1) % 3 == 0 and (i+1) % 4 !=0 :
+        print("Fizz")
+    elif (i+1) % 4 == 0 and (i+1) % 3 !=0:
+        print ("BUZZ")
     else:
-   		print (i+1)
+        print (i+1)
